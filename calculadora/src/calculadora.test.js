e6fbce95a0c95e9ef0021f866c08c2c1d91fb1cd
tenorionique <monique_doretto@hotmail.com>
2021-11-01 21:49:46 -0300

    feat(teste) testa as operações da calculadora e suas funções

--- calculadora/src/calculadora.test.js
+++ calculadora/src/calculadora.test.js
@@ -1,8 +1,69 @@
-/*import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import Calculadora from './calculadora';
 
-test('renders learn react link', () => {
-  render(<Calculadora />);
-  const linkElement = screen.getByText(/calculadora/i);
-  expect(linkElement).toBeInTheDocument();
-});*/
+
+describe('Calculadora', () => {
+
+  it('deve renderizar o component sem erros', () => {
+    const div = document.createElement('div');
+    render(<Calculadora />, div);
+  });
+
+  it('deve limpar o campo de numeros', () => {
+    const { getByTestId, getByText } = render(<Calculadora />);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('C'));
+    expect(getByTestId('txtNumeros')).toHaveValue('0');
+  });
+
+  it('deve somar 2 + 3 e obter 5', () => {
+    const { getByTestId, getByText } = render(<Calculadora />);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('+'));
+    fireEvent.click(getByText('3'));
+    fireEvent.click(getByText('='));
+    expect(getByTestId('txtNumeros')).toHaveValue('5');
+  });
+
+  it('deve somar 2.5 + 3 e obter 5.5', () => {
+    const { getByTestId, getByText } = render(<Calculadora />);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('.'));
+    fireEvent.click(getByText('5'));
+    fireEvent.click(getByText('+'));
+    fireEvent.click(getByText('3'));
+    fireEvent.click(getByText('='));
+    expect(getByTestId('txtNumeros')).toHaveValue('5.5');
+  });
+
+  it('deve subtrair 2 - 3 e obter -1', () => {
+    const { getByTestId, getByText } = render(<Calculadora />);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('-'));
+    fireEvent.click(getByText('3'));
+    fireEvent.click(getByText('='));
+    expect(getByTestId('txtNumeros')).toHaveValue('-1');
+  });
+
+  it('deve dividir 2 / 3 e obter 0.6666666666666666', () => {
+    const { getByTestId, getByText } = render(<Calculadora />);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('/'));
+    fireEvent.click(getByText('3'));
+    fireEvent.click(getByText('='));
+    expect(getByTestId('txtNumeros')).toHaveValue('0.6666666666666666');
+  });
+
+  it('deve multiplicar 2 * 3 e obter 6', () => {
+    const { getByTestId, getByText } = render(<Calculadora />);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('*'));
+    fireEvent.click(getByText('3'));
+    fireEvent.click(getByText('='));
+    expect(getByTestId('txtNumeros')).toHaveValue('6');
+  });
+
+
+})
+
